307c9de14fe8729554fb4a997c2eb6f0d7d5c723
all4outgamingyogesh <anjaliyadav26030.4@gmail.com>
2024-09-13 12:09:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
+mkdir -p .devcontainer
+cat <<EOL > .devcontainer/devcontainer.json
+{
+    "name": "My Codespace",
+    "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
+    "features": {
+        "ghcr.io/devcontainers/features/sshd:1": {
+            "version": "latest"
+        }
+    },
+    "postStartCommand": "python3 /workspaces/danger/main.py",
+    "customizations": {
+        "vscode": {
+            "settings": {
+                "python.pythonPath": "/usr/local/bin/python"
+            },
+            "extensions": [
+                "ms-python.python"
+            ]
+        }
+    }
+}
 
+EOL
+
+git add .devcontainer/devcontainer.json
+git commit -m "Update postStartCommand to run main.py automatically"
+git push origin main
